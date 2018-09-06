171d5d37fea66fded75144f0213c98744825e863
Elissa Fink <elissafink@MacBook-Pro-10.ucsf.edu>
2018-09-06 10:08:16 -0700

    script

---
+++ fastq_to_ham_plot.py
@@ -0,0 +1,21 @@
+# Import a FASTQ file, calculate the hamming distances for each sequence,
+# and plot these distances in a way you think is useful.
+
+# Script must ultimately be contained in one fluid .py file merged via GitHub
+
+import matplotlib.pyplot as plt
+
+f = open(CTGATC.fastq)
+
+def getSeqs(fastq_file):
+	#Parse a FASTQ for sequence identities and corresponding sequences
+	seqences = {}
+	return sequences
+
+def hamDist(str1, str2):
+   #Count the # of differences between equal length strings str1 and str2
+   diffs = 0
+   return diffs
+
+#Make some kind of plot that contains the data you've calculated.
+plt.show()
