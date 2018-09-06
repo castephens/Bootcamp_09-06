451b651ddc3b0d193fcb3f91df3b0b4010c034e4
castephens <castephens@email.wm.edu>
2018-09-06 11:41:49 -0700

    ok

--- fastq_to_ham_plot.py
+++ fastq_to_ham_plot.py
@@ -5,17 +5,48 @@
 
 import matplotlib.pyplot as plt
 
-f = open(CTGATC.fastq)
+file = "CTGATC.fastq"
 
 def getSeqs(fastq_file):
-	#Parse a FASTQ for sequence identities and corresponding sequences
-	seqences = {}
-	return sequences
+    #Parse a FASTQ for sequence identities and corresponding sequences
+    sequences = {}
+    count = 1
+    with open(fastq_file, 'r') as f:
+        for line in f:
+            line = line.rstrip('\n')
+            if "@" not in line and "<" not in line and "#" not in line and "+" not in line:
+                sequences[count] = line
+                count += 1
+            
+    return(sequences)
 
 def hamDist(str1, str2):
    #Count the # of differences between equal length strings str1 and str2
-   diffs = 0
-   return diffs
+    str1list = list(str1)
+    str2list = list(str2)
+    diff = 0
+    for i in range(1,len(str1list)+1):
+        if str1list[i-1] == str2list[i-1]:    
+            continue
+        else:
+            diff+=1
+    
+    return(diff)
+
+sequences = getSeqs(file)
+all_diffs = []
+#for i in range(1,len(sequences.keys())+1):
+for i in range(1,500):
+        print(sequences[i] )
+        seq1 = sequences[i] 
+        #for j in range(1,len(sequences.keys())+1):
+        for j in range(1,500):
+            if i != j and j < len(sequences.keys()):
+                seq2 = sequences[j+1]
+                if len(seq1) == len(seq2):
+                    all_diffs.append(hamDist(seq1,seq2))
+
+print(len(all_diffs))           
 
 #Make some kind of plot that contains the data you've calculated.
-plt.show()
+#plt.show()
